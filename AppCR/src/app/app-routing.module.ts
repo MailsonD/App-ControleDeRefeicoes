43ef8edee97ecde0f2e3d3abaadd24d0193edc05
Raul-Coelho <raulcc50@gmail.com>
2019-09-05 17:00:47 -0300

    Configurando rota

--- AppCR/src/app/app-routing.module.ts
+++ AppCR/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'view-publica', loadChildren: './pages/view-publica/view-publica.module#ViewPublicaPageModule'},
 ];
 
 @NgModule({
