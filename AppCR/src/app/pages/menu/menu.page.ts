76cfd7cb41990c332e135a1d2e998cd7a833fbb0
Raul-Coelho <raulcc50@gmail.com>
2019-09-07 13:48:20 -0300

    Criando side-menu

---
+++ AppCR/src/app/pages/menu/menu.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  
+
+  pages = [
+    {
+      title: 'Main',
+      url: '/menu/estatistica-gestor',
+      icon: 'home'
+    },
+    {
+      title: 'Logout',
+      url: '/tabs/tab1/view',
+      icon: 'power'
+    }
+  ]
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+  }
+
+}
