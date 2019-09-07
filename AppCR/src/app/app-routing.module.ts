8fab6880174040b5669efe9c9ea56882bf180105
Raul-Coelho <raulcc50@gmail.com>
2019-09-07 13:46:17 -0300

    Configurando rotas

--- AppCR/src/app/app-routing.module.ts
+++ AppCR/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'view-publica', loadChildren: './pages/view-publica/view-publica.module#ViewPublicaPageModule'},
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  { path: 'view-publica', loadChildren: './pages/view-publica/view-publica.module#ViewPublicaPageModule' },
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'view-almoco', loadChildren: './pages/view-almoco/view-almoco.module#ViewAlmocoPageModule' },
   { path: 'view-janta', loadChildren: './pages/view-janta/view-janta.module#ViewJantaPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' }
 ];
 
 @NgModule({
