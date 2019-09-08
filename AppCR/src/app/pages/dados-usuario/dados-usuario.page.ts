77b339ca3d14d21c879bbbf42d264ddddacaedcb
caiquevitoriano <vitorianocaique@gmail.com>
2019-09-07 21:58:27 -0300

    api camera

--- AppCR/src/app/pages/dados-usuario/dados-usuario.page.ts
+++ AppCR/src/app/pages/dados-usuario/dados-usuario.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+
 @Component({
   selector: 'app-dados-usuario',
   templateUrl: './dados-usuario.page.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DadosUsuarioPage implements OnInit {
 
-  constructor() { }
+  capturedSnapURL:string;
+
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  constructor(private camera: Camera) { }
 
   ngOnInit() {
+  }
+
+  takeSnap() {
+    this.camera.getPicture(this.cameraOptions).then((imageData) => {
+      // this.camera.DestinationType.FILE_URI gives file URI saved in local
+      // this.camera.DestinationType.DATA_URL gives base64 URI
+      
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.capturedSnapURL = base64Image;
+    }, (err) => {
+      
+      console.log(err);
+      // Handle error
+    });
   }
 
 }
