d9cae210eddd2f7e5f42082db6bc784401019b1e
Raul-Coelho <raulcc50@gmail.com>
2019-09-09 16:05:11 -0300

    Finalizando parcialmente pagina de historico de solocitações

--- AppCR/src/app/Professor/historico/historico.page.ts
+++ AppCR/src/app/Professor/historico/historico.page.ts
@@ -5,11 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './historico.page.html',
   styleUrls: ['./historico.page.scss'],
 })
-export class HistoricoPage implements OnInit {
 
-  constructor() { }
+
+export class HistoricoPage implements OnInit{
+
+  private turmas: any[];
+
+  private cor: string;
+
+  constructor() { 
+    this.turmas = [
+      {
+        nome:'ADS',
+        status:'Aprovado',
+        data:'06/06/2006',
+        quant:'600'
+      },
+      {
+        nome:'Eng',
+        status:'Recusado',
+        data:'12/12/2012',
+        quant:'111'
+      },
+      {
+        nome:'Auto',
+        status:'Recusado',
+        data:'15/15/2015',
+        quant:'60'
+      }
+    ]
+
+  }
 
   ngOnInit() {
+    
   }
 
 }
