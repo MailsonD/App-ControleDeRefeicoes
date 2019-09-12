1d61656629ae09c578807773d5647b4d1f43795e
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-09-12 17:30:34 -0300

    Persistencia da sessao de usuário

---
+++ AppCR/src/app/guards/back-auth.guard.ts
@@ -0,0 +1,46 @@
+import { environment } from './../../environments/environment';
+import { Platform } from '@ionic/angular';
+import { AppPreferences } from '@ionic-native/app-preferences/ngx';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackAuthGuard implements CanActivate {
+
+  constructor(
+    private appPreferences: AppPreferences,
+    private router: Router,
+    private platform: Platform
+  ) { };
+
+  async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    let sessao: boolean = false;
+    if (!this.platform.is('mobileweb')) {
+      //uso mobile
+      await this.appPreferences.fetch(environment.SHARED_PREFERENCES_DIRECTORY, environment.SESSAO_ATIVA).then(async autenticado => {
+        sessao = autenticado;
+        console.log("valor preferences");
+        console.log(autenticado);
+        await this.appPreferences.fetch(environment.SHARED_PREFERENCES_DIRECTORY, environment.TIPO_ACESSO).then(tipoAcesso => {
+          if(tipoAcesso === 'PROFESSOR'){
+            this.router.navigate(['menu-prof/solicitacao']);
+            console.log(tipoAcesso);
+            console.log("SharedPreferences: redirecionando para pagina de professor");
+          }else if(tipoAcesso === 'GESTOR'){
+            this.router.navigate(['estatisticas']);
+            console.log(tipoAcesso);
+            console.log("SharedPreferences: redirecionando para pagina de gestor");
+          }
+        })
+      }).catch(err => {
+        console.log(err); console.log("Erro no Auth")
+      });
+      return !sessao;
+    }
+    //uso do navegador
+    return true;
+  }
+
+}
--- AppCR/src/app/services/session.service.ts
+++ AppCR/src/app/services/session.service.ts
@@ -1,6 +1,8 @@
+import { environment } from './../../environments/environment';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Usuario } from './../models/Usuario';
 import { Injectable } from '@angular/core';
+import { AppPreferences } from '@ionic-native/app-preferences/ngx';
 
 /**
  * @author leanderson.coelhoif@gmail.com
@@ -12,15 +14,29 @@ import { Injectable } from '@angular/core';
 export class SessionService {
 
   private _usuario: BehaviorSubject<Usuario> = new BehaviorSubject<Usuario>({});
-  $usuario: Observable<Usuario> = this._usuario.asObservable(); 
+  $usuario: Observable<Usuario> = this._usuario.asObservable();
 
-  constructor() { }
+  constructor(
+    private appPreferences: AppPreferences
+  ) { }
 
-  createSession(usuario: Usuario): void{
+  createSession(usuario: Usuario): void {
     this._usuario.next(usuario);
+    this.appPreferences.store(environment.SHARED_PREFERENCES_DIRECTORY, environment.SESSAO_ATIVA, true)
+      .then(() => console.log("SharedPreferences: Sessao iniciada."))
+      .catch(err => console.log("SharedPreferences: Falha: ", err));
+    this.appPreferences.store(environment.SHARED_PREFERENCES_DIRECTORY, environment.TIPO_ACESSO, usuario.nivelAcesso)
+      .then(() => console.log("SharedPreferences: Tipo de acesso iniciada."))
+      .catch(err => console.log("SharedPreferences: Falha: ", err));
   }
 
-  invalidateSession(): void{
+  invalidateSession(): void {
     this._usuario.next(null);
+    this.appPreferences.remove(environment.SHARED_PREFERENCES_DIRECTORY, environment.SESSAO_ATIVA)
+      .then(() => console.log("SharedPreferences: Sessao finalizada."))
+      .catch(err => console.log("SharedPreferences: Falha: ", err));
+    this.appPreferences.remove(environment.SHARED_PREFERENCES_DIRECTORY, environment.TIPO_ACESSO)
+      .then(() => console.log("SharedPreferences: Tipo de acesso iniciada."))
+      .catch(err => console.log("SharedPreferences: Falha: ", err));
   }
 }
